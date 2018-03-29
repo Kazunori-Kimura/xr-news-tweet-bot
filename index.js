7d266031e98656146e08ad0ab62a478618ce9369
Kazunori.Kimura <kazunori.kimura.js@gmail.com>
2018-03-29 17:15:22 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// index.js
+require("dotenv").config();
+const Article = require("./lib/Article");
+const Twitter = require("twitter");
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+(async () => {
+  try {
+    const article = await Article.pop();
+    const message = await article.message();
+
+    const tweet = await client.post("statuses/update", { status: message });
+    console.log(tweet);
+    
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+})();
---
+++ test/Article.test.js
@@ -0,0 +1,20 @@
+const Article = require("../lib/Article");
+
+describe("Article", () => {
+  /**
+   * @type {Article}
+   */
+  let article;
+
+  test("pop()", async () => {
+    expect.assertions(1);
+    article = await Article.pop();
+    expect(article).toBeInstanceOf(Article);
+  });
+
+  test("message()", async () => {
+    expect.assertions(1);
+    const message = await article.message();
+    expect(message.length).toBeLessThanOrEqual(140);
+  });
+});
